41c9a20d8b599099aef017ea63a554e9995183c1
Shandley Phillips <shandley.phillips@sharp.com>
2016-12-30 21:40:50 -0800

    about us content

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,7 +9,7 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  aboutContent: string;
   constructor(private seoService: SeoService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
@@ -18,6 +18,8 @@ export class AboutComponent implements OnInit {
       this.seoService.setMetaDescription('About Desciption for - TIWed Inc.');
       this.seoService.setCanonicalLink('http://www.tiwedinc.com/about');
       this.seoService.setMetaRobots('index');
+
+      aboutContent = 'TI Wed, Inc. was started by several developers in San Diego with tons of Angular experience.  They we tasked with one of the most difficult tasks ever handed to front-end developers.  They were tasked with photoshopping the heads of several managerial individuals to the heads of PokeMon characters for a company celebration.  While this seems like an almost impossible task, the two developers were behind the eight ball from the begining.  They didn\'t have PhotoShop.  From then on they were constently bombarded with reminders of their deadlines.  Wednesday.  Until finally they were sent an email with 135px font reminding them that tomorrow is wednesday.  TI Wed was born.  Since then, TI Wed has been built Angular apps while working closely with industry experts.  This site is one of many experiments in how Google crawls angular sites.';
     });
   }
 
